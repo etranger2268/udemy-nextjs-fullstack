1257ad6fbc7de382761f93832e015b6ea1fcf1b6
etranger2268 <shun.5.2268@gmail.com>
2025-12-22 19:25:19 +0900

    develop-2-06

---
+++ src/app/task/edit/page.tsx
@@ -0,0 +1,3 @@
+export default function TaskEditPage() {
+  return <h1 className="text-red-500 text-2xl">TaskEditPage</h1>;
+}
---
+++ src/app/task/page.tsx
@@ -0,0 +1,3 @@
+export default function TaskPage() {
+  return <h1 className="text-red-500 text-2xl">TaskPage</h1>;
+}
