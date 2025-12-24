42186ff1dd3f3eb24d5184009bce2a6acb4e7989
etranger2268 <shun.5.2268@gmail.com>
2025-12-24 16:31:00 +0900

    develop-3-28

---
+++ src/app/(main)/completed/page.tsx
@@ -0,0 +1,14 @@
+import TaskCard from '@/app/(main)/_components/TaskCard/TaskCard';
+
+export default function CompletedTask() {
+  return (
+    <div className="text-gray-800 p-8 h-full overflow-y-auto pb-24">
+      <header className="flex j">
+        <h1 className="text-3xl font-bold flex items-center">Completed Tasks</h1>
+      </header>
+      <div className="mt-8 flex flex-wrap gap-4">
+        <TaskCard />
+      </div>
+    </div>
+  );
+}
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -6,10 +6,10 @@ export default function Home() {
   return (
     <div className="text-gray-800 p-8 h-full overflow-y-auto pb-24">
       <header className="flex justify-between items-center">
-        <h1 className="text-2xl font-bold flex items-center">All Tasks</h1>
+        <h1 className="text-3xl font-bold flex items-center">All Tasks</h1>
         <Link
           href="/new"
-          className="flex items-center gap-1 font-semibold border px-3 py-1.5 rounded-full shadow-sm transition-all duration-300 bg-gray-800 text-white hover:bg-gray-700"
+          className="flex items-center gap-1 font-semibold border px-3 py-1 rounded-full shadow-sm transition-all duration-300 bg-gray-800 text-white hover:bg-gray-700"
         >
           <MdAddTask className="size-5" />
           <div>Add Task</div>
