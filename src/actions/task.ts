25ae03abe13f5f0e52dc71bab643510e3bc52db5
etranger2268 <shun.5.2268@gmail.com>
2025-12-25 13:40:13 +0900

    develop-4-37

---
+++ src/actions/task.ts
@@ -0,0 +1,31 @@
+'use server';
+
+import { redirect } from 'next/navigation';
+import { type Task, TaskModel } from '@/models/task';
+import { connectDb } from '@/utils/database';
+
+export type FormState = {
+  error: string;
+};
+
+export const createTask = async (state: FormState, formData: FormData) => {
+  const newTask: Task = {
+    title: formData.get('title') as string,
+    description: formData.get('description') as string,
+    dueDate: formData.get('dueDate') as string,
+    isCompleted: false,
+  };
+
+  try {
+    await connectDb();
+    await TaskModel.create(newTask);
+  } catch (e) {
+    console.log(e);
+    return {
+      ...state,
+      error: 'タスクの作成に失敗しました',
+    };
+  }
+
+  redirect('/');
+};
